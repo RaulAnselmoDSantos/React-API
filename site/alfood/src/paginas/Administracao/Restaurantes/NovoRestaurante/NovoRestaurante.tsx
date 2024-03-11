ab2da589277e68df13c3c4c9feaafadd677cfe27
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-03-11 12:42:04 -0300

    feat: new pages of admin manegement, new restaurant and list of restaurant

---
+++ site/alfood/src/paginas/Administracao/Restaurantes/NovoRestaurante/NovoRestaurante.tsx
@@ -0,0 +1,37 @@
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+import IRestaurante from "../../../../interfaces/IRestaurante";
+
+const NovoRestaurante = () => {
+  const [nomeRestaurante, setNomeRestaurante] = useState("");
+
+  const onSubmiterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    
+    axios.post<IRestaurante>('http://localhost:8000/api/v2/restaurantes/', 
+        {  
+            nome: nomeRestaurante
+        })
+            .then( () => {
+                alert("Restaurante Cadastrado");
+            })
+  };
+
+  return (
+    <form onSubmit={onSubmiterForm}>
+      <TextField
+        value={nomeRestaurante}
+        onChange={(evento) => setNomeRestaurante(evento.target.value)}
+        id="outlined-basic"
+        label="Nome do Restaurante"
+        variant="outlined"
+      />
+      <Button type="submit" variant="outlined">
+        Salvar
+      </Button>
+    </form>
+  );
+};
+
+export default NovoRestaurante;
