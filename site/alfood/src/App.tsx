ba732aed7f78bc650ea9aa4833e14b489c29dbee
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-03-19 14:14:44 -0300

    feat: add of the route /admin in the App.tsx using router-dom

--- site/alfood/src/App.tsx
+++ site/alfood/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
 import NovoRestaurante from './paginas/Administracao/Restaurantes/NovoRestaurante/NovoRestaurante';
+import Admin from './paginas/Administracao/Admin';
 
 function App() {
 
@@ -10,9 +11,12 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-      <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
-      <Route path="/admin/restaurantes/Novo" element={<NovoRestaurante />} />
-      <Route path="/admin/restaurantes/:id" element={<NovoRestaurante />} />
+      <Route path='/admin' element={<Admin />}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/Novo" element={<NovoRestaurante />} />
+        <Route path="restaurantes/:id" element={<NovoRestaurante />} />
+      </Route>
+
     </Routes>
   );
 }
